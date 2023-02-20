00f1c0c114d45e935131e468f2f01339561a3eb8
FangCai-fifi <fangcai.fifi@gmail.com>
2023-02-20 15:26:22 +0800

    trac2obj

--- trac2obj.py
+++ trac2obj.py
@@ -4,8 +4,8 @@ import numpy as np
 import nibabel as nib
 import utils
 
-SUBDIR = '/Users/fangcai/traculaData/diffusion_tutorial/trc' # replace it with your recon-all package path
-SUBNAME = 'PZH' # replace it with your subject id
+SUBDIR = '/Users/fangcai/traculaData/diffusion_tutorial/trc' # replace it with your trac-all package path
+SUBNAME = 'example' # replace it with your subject id
 TRKLIST = ['lh.atr', 'lh.cab', 'lh.ccg', 'lh.cst', 'lh.ilf', 'lh.slfp', 'lh.slft', 'lh.unc',
            'rh.atr', 'rh.cab', 'rh.ccg', 'rh.cst', 'rh.ilf', 'rh.slfp', 'rh.slft', 'rh.unc' ]
 

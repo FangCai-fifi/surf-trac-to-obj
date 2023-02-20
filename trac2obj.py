de2ddabe009d847c6185d9a384990b6be11dedb9
FangCai-fifi <fangcai.fifi@gmail.com>
2023-02-20 15:23:03 +0800

    trac2obj added

---
+++ trac2obj.py
@@ -0,0 +1,20 @@
+import os
+import re
+import numpy as np
+import nibabel as nib
+import utils
+
+SUBDIR = '/Users/fangcai/traculaData/diffusion_tutorial/trc' # replace it with your recon-all package path
+SUBNAME = 'PZH' # replace it with your subject id
+TRKLIST = ['lh.atr', 'lh.cab', 'lh.ccg', 'lh.cst', 'lh.ilf', 'lh.slfp', 'lh.slft', 'lh.unc',
+           'rh.atr', 'rh.cab', 'rh.ccg', 'rh.cst', 'rh.ilf', 'rh.slfp', 'rh.slft', 'rh.unc' ]
+
+trkPath = os.path.join(SUBDIR, SUBNAME, 'dpath')
+for root, dirs, files in os.walk(trkPath, topdown=True):
+    for dirname in dirs:
+        for item in TRKLIST:
+            if re.search(item, dirname): # if found preprocessed!
+                dpath = os.path.join(trkPath, dirname, 'path.pd.trk')
+                print(dpath)
+                ver, face = utils.read_trk(path=nib.streamlines.load(dpath))
+                utils.write_obj(ver, face, objPath=f"assets/{item}.obj", plusOne=False)
--- utils.py
+++ utils.py
@@ -31,6 +31,8 @@ def write_obj(ver, face, objPath, plusOne=True):
     
     if plusOne:
         face1 = 1 + face
+    else:
+        face1 = np.copy(face)
     
     thefile = open(objPath, 'w')
     
@@ -40,3 +42,35 @@ def write_obj(ver, face, objPath, plusOne=True):
         thefile.write("f {0} {1} {2}\n".format(item[0],item[1],item[2]))  
         
     thefile.close()
+
+
+def read_trk(path):
+    tracNum = 0
+    verNum = 0
+    ver = np.array([0, 0, 0])
+    verNum_list = []
+    for item in path.streamlines:
+        verNum += item.shape[0]
+        verNum_list.append(item.shape[0])
+        tracNum += 1
+        ver = np.vstack((ver, item))
+    ver = ver[1:, :]
+    
+    ver_4d = np.hstack((ver, np.ones((ver.shape[0],1))))
+    ver_vox = np.matmul(np.linalg.inv(path.affine), np.transpose(ver_4d))
+    trkvox2tkras = np.array([[-1, 0, 0, 128], [0, 1, 0, -128], [0, 0, 1, -127], [0, 0, 0, 1]]) # trk pkg is always LAS orientation!!!
+    ver_tkras = np.transpose(np.matmul(trkvox2tkras, ver_vox)[0:3, :])
+    
+    arrLen = verNum_list[0] - 2
+    face = np.transpose(np.vstack((1 + np.arange(arrLen), 2 + np.arange(arrLen), 3 + np.arange(arrLen))))
+    arrPre = verNum_list[0]
+    for i in range(tracNum-1):
+        arrLen = verNum_list[i+1] - 2
+        f1 = 1 + arrPre + np.arange(arrLen)
+        f2 = 2 + arrPre + np.arange(arrLen)
+        f3 = 3 + arrPre + np.arange(arrLen)
+        face_tmp = np.transpose(np.vstack((f1, f2, f3)))
+        face = np.vstack((face, face_tmp))
+        arrPre += verNum_list[i+1]
+    
+    return ver_tkras, face
